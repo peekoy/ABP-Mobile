1cfb553a909cb87539597c3d291e18d20b7f4dcf
Deore Mufrad Hendrady <118089389+mufrad2004@users.noreply.github.com>
2025-03-27 13:18:00 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/login_page.dart';
-import 'screens/register_page.dart';
+import 'login_page.dart';
+import 'register_page.dart';
 
 void main() {
   runApp(const MyApp());
